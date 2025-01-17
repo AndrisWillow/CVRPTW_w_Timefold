320b510a2e96d8966e285dadd4444722492d2b0d
AndrisWillow <andris.vitols@gmail.com>
2025-01-17 10:46:42 +0200

    Added simulated annealing as an optimizer

--- src/vehicle_routing/solver.py
+++ src/vehicle_routing/solver.py
@@ -1,21 +1,40 @@
-from timefold.solver import SolverManager, SolutionManager
+from timefold.solver import SolverManager, SolutionManager, SolverFactory
 from timefold.solver.config import (SolverConfig, ScoreDirectorFactoryConfig,
                                     TerminationConfig, Duration)
-
+from pathlib import Path
 from .domain import *
 from .constraints import define_constraints
 
+xml_path = Path(__file__).parent / "solver_configs" / "simulated_annealing.xml"
+solver_config = SolverConfig.create_from_xml_resource(str(xml_path))
 
-solver_config = SolverConfig(
-    solution_class=VehicleRoutePlan,
-    entity_class_list=[Vehicle, Visit],
-    score_director_factory_config=ScoreDirectorFactoryConfig(
-        constraint_provider_function=define_constraints
-    ),
-    termination_config=TerminationConfig(
-        spent_limit=Duration(seconds=30)
-    )
+# Some overrides to the config:
+termination_config = TerminationConfig(
+    # Can use user input as steps
+    spent_limit=Duration(seconds=30)
 )
+solver_config.termination_config = termination_config
+
+score_director_config = ScoreDirectorFactoryConfig(
+    constraint_provider_function=define_constraints
+)
+solver_config.score_director_factory_config = score_director_config
 
 solver_manager = SolverManager.create(solver_config)
 solution_manager = SolutionManager.create(solver_manager)
+
+# Old solver config. TODO what does the optimizer use by default?
+# solver_config = SolverConfig(
+#     solution_class=VehicleRoutePlan,
+#     entity_class_list=[Vehicle, Visit],
+#     score_director_factory_config=ScoreDirectorFactoryConfig(
+#         constraint_provider_function=define_constraints
+#     ),
+#     # Maybe we can do steps instead
+#     termination_config=TerminationConfig(
+#         spent_limit=Duration(seconds=30)
+#     )
+# )
+
+# solver_manager = SolverManager.create(solver_config)
+# solution_manager = SolutionManager.create(solver_manager)
